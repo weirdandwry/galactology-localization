868b6a618e63dc91b600c6c5f61748786b331703
Carlos Carrasco <carloscm@gmail.com>
2018-02-01 00:07:58 +0100

    wip turkish and german

---
+++ mods/local-de/manifest.lua
@@ -0,0 +1,7 @@
+return {
+    name = "Deutsch",
+    description = "Deutsche Übersetzung.",
+    api_version = 4,
+    -- please use ISO 639-1 plus country code if required
+    language_code = "de"
+}
---
+++ mods/local-tr/manifest.lua
@@ -0,0 +1,7 @@
+return {
+    name = "Türkçe",
+    description = "Türkçe çeviri.",
+    api_version = 4,
+    -- please use ISO 639-1 plus country code if required
+    language_code = "tr"
+}
