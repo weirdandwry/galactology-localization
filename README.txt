bfd7c61150585e20b9fa9e3acf068c6a7e409969
Carlos Carrasco <carloscm@gmail.com>
2018-01-31 15:49:06 +0100

    initial commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+*** Master localization files and official translations for The Spatials: Galactology ***
+
+Visit https://translate.weirdandwry.com/ to contribute to Galactology translations.
---
+++ mods/local-es/manifest.lua
@@ -0,0 +1,7 @@
+return {
+    name = "Español",
+    description = "Traducción al Español. Por favor reinicia el juego para aplicar los cambios de idioma.",
+    api_version = 4,
+    -- please use ISO 639-1 plus country code if required
+    language_code = "es"
+}
