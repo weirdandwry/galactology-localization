cd227c618d5b80c0443b583e79e0663f40329be2
Carlos Carrasco <carloscm@gmail.com>
2018-02-01 11:37:55 +0100

    russian

---
+++ mods/local-ru/manifest.lua
@@ -0,0 +1,7 @@
+return {
+    name = "Русский",
+    description = "Перевод на русский язык.",
+    api_version = 4,
+    -- please use ISO 639-1 plus country code if required
+    language_code = "ru"
+}
