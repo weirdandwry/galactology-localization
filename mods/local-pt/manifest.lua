2c779911222115b293c6891e588f48ad7d603c81
Carlos Carrasco <carloscm@gmail.com>
2018-02-13 13:44:33 +0100

    add portuguese

---
+++ mods/local-pt/manifest.lua
@@ -0,0 +1,9 @@
+return {
+    name = "Português [Built-in]",
+    description = "Tradução em português.",
+    api_version = 4,
+    -- incomplete
+    skip = true,
+    -- please use ISO 639-1 plus country code if required
+    language_code = "pt"
+}
