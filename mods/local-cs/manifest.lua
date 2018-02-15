555d73bbee0c20492650b59e827c589a515cdfc9
Carlos Carrasco <carloscm@gmail.com>
2018-02-15 11:19:51 +0100

    add czech

---
+++ mods/local-cs/manifest.lua
@@ -0,0 +1,9 @@
+return {
+    name = "Česky [Built-in]",
+    description = "Překlad do češtiny.",
+    api_version = 4,
+    -- incomplete
+    skip = true,
+    -- please use ISO 639-1 plus country code if required
+    language_code = "cs"
+}
