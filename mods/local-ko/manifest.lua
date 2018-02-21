45684ced8cfadafea35ad1373ee6455f1c9c14fa
Carlos Carrasco <carloscm@gmail.com>
2018-02-21 17:54:51 +0100

    add korean

---
+++ mods/local-ko/manifest.lua
@@ -0,0 +1,10 @@
+return {
+    name = "Korean [Built-in]",
+    description = "Translation for Korean. Korean font is not activated until this mod is selected.",
+    api_version = 4,
+    fallback_font = "font/NotoSansKR-Regular.otf",
+    -- incomplete
+    skip = true,
+    -- please use ISO 639-1 plus country code if required
+    language_code = "ko"
+}
