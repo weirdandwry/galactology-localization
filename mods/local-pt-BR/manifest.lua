37404afe2517b9d8d2356878af82614c9d53f085
Carlos Carrasco <carloscm@gmail.com>
2018-02-27 11:08:21 +0100

    add pt br

---
+++ mods/local-pt-BR/manifest.lua
@@ -0,0 +1,9 @@
+return {
+    name = "Português (Brasil) [Built-in]",
+    description = "Tradução em português (Brasil).",
+    api_version = 4,
+    -- incomplete
+    skip = true,
+    -- please use ISO 639-1 plus country code if required
+    language_code = "pt-BR"
+}
